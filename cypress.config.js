4768b6b2dfc3716c541a4a7ff13edc5d4cd5f370
bowed89 <jesus.lopez.averanga@gmail.com>
2025-07-01 23:57:25 -0400

    E2E test Deployment pipeline2

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,8 @@
-const { defineConfig } = require('cypress')
-
+const { defineConfig } = require("cypress");
 module.exports = defineConfig({
- e2e: {
-    baseUrl: 'http://localhost:5000',
+  e2e: {
+    baseUrl: "http://localhost:5000",
     supportFile: false,
   },
-  video: false
-})
+  video: false,
+});
