0f01c9a5b7d31f690f7d64aeae592165a85b701d
bowed89 <jesus.lopez.averanga@gmail.com>
2025-07-03 23:53:05 -0400

    Cambios para realizar Deploy en GithubPages 14

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/pokedex-for-ci/', // este es el cambio clave
   },
   module: {
     rules: [
