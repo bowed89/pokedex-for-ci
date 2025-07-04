cb29961cae22326d45a878a83ec46ccf3b4f24de
bowed89 <jesus.lopez.averanga@gmail.com>
2025-07-03 23:27:38 -0400

    Cambios para realizar Deploy en GithubPages 10

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -3,6 +3,6 @@ describe('Pokedex', function () {
   it('frontpage can be opened', function () {
     cy.visit('/')
     cy.contains('pikachu')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo') 
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo')
   })
 })
